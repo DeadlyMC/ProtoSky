5a697b02877e4ecdf422e6544d65aca76635511a
CheaterCodes <cheater.codes@outlook.com>
2020-07-28 00:42:39 +0200

    use proper structure spawning

--- src/main/java/protosky/gen/WorldGenUtils.java
+++ src/main/java/protosky/gen/WorldGenUtils.java
@@ -1,15 +1,20 @@
 package protosky.gen;
 
+import com.google.common.collect.ImmutableList;
 import net.minecraft.block.BlockState;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.NbtHelper;
+import net.minecraft.server.network.SpawnLocating;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.Structure;
 import net.minecraft.structure.StructureManager;
+import net.minecraft.structure.StructurePlacementData;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.PackedIntegerArray;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
@@ -17,6 +22,7 @@ import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.chunk.ProtoChunk;
 import net.minecraft.world.chunk.WorldChunk;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
 import protosky.mixins.ProtoChunkAccessor;
 
 import java.util.*;
@@ -74,43 +80,7 @@ public class WorldGenUtils
         if (s == null) return;
         CompoundTag t = new CompoundTag();
 
-        // s.place would call world.getChunk, causing it to hang.
-        // Hence, I've reimplemented spawning a structure.
-        // The limitation here is that the structure is limited to 16 blocks,
-        // and assumes it centers around (8, 0, 8).
-        // Other limitations for this implementation are:
-        // - No support for BlockEntities
-        // - No support for Entities
-        // Additionally, this platform is spawned at Y=64 with no way to configure this.
-
-        s.toTag(t);
-
-        // Collect the palette
-        Map<Integer, BlockState> map = new HashMap<>();
-        ListTag palette = t.getList("palette", 10);
-        for (int i = 0; i < palette.size(); i++) {
-            CompoundTag e = palette.getCompound(i);
-            map.put(i, NbtHelper.toBlockState(e));
-        }
-        BlockPos.Mutable relStart = chunk.getPos().getCenterBlockPos().mutableCopy();
-        relStart.setY(64);
-
-        // Place the blocks in the chunk
-        BlockPos highest = BlockPos.ORIGIN;
-        ListTag blocks = t.getList("blocks", 10);
-        for (int i = 0; i < blocks.size(); i++) {
-            CompoundTag b = blocks.getCompound(i);
-            BlockState st = map.get(b.getInt("state"));
-            ListTag t2 = b.getList("pos", 3);
-            BlockPos p = relStart.add(new BlockPos(t2.getInt(0), t2.getInt(1), t2.getInt(2)));
-            if (p.getY() > highest.getY()) {
-                highest = p;
-            }
-            chunk.setBlockState(p, st, false);
-        }
-
-        // Because of the way we spawn the structure, it's common to fall into the void next to it.
-        // As solution, we simply set the world spawn on top of said structure.
-        world.setSpawnPos(highest.up());
+        s.place(new ChunkRegion(world, ImmutableList.of(chunk)), chunk.getPos().toBlockPos(0,64,0), new StructurePlacementData().setUpdateNeighbors(true), new Random());
+        world.setSpawnPos(chunk.getPos().toBlockPos(s.getSize().getX() / 2, 64 + s.getSize().getY() + 1, s.getSize().getZ() / 2));
     }
 }
